283721215ba58a00ca190b0a78e07bd32a49d76c
hwalker928 <mail@harrydev.org>
2021-03-10 10:11:14 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Plugin } = require('powercord/entities');
 
 module.exports = class motivationPlugin extends Plugin {
-  
+
   startPlugin () {
     powercord.api.commands.registerCommand({
       command: 'motivation',
@@ -10,17 +10,17 @@ module.exports = class motivationPlugin extends Plugin {
       executor: this.quote.bind(this)
     });
   }
-  
+
     pluginWillUnload() {
         powercord.api.commands.unregisterCommand('motivation');
     }
-  
+
   async quote (args) {
     const data = await get(`http://api.quotable.io/random`).then(r => r.data);
     return {
-      true,
+      send: true,
       result: data['content']
     };
   }
-  
+
 };
